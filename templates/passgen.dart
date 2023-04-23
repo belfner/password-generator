4e8d6a653a5976467dfcceb4f4ebf702f981fb19
belfner <belfner@belfner.com>
2023-04-23 19:05:02 -0400

    made _random part of the Passgen class

--- templates/passgen.dart
+++ templates/passgen.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 
-final _random = new Random();
-
 class Passgen {
+  static final _random = new Random();
   static List<String> words = [$words];
   static List<String> upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   static List<String> digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -10,12 +9,12 @@ class Passgen {
   static String genPass(int numWords) {
     String password = '';
     for (var i = 0; i < numWords - 1; i++) {
-      password += Passgen.words[_random.nextInt(Passgen.words.length)];
+      password += Passgen.words[Passgen._random.nextInt(Passgen.words.length)];
       password += '.';
     }
-    password += Passgen.words[_random.nextInt(Passgen.words.length)];
-    password += Passgen.upperLetters[_random.nextInt(Passgen.upperLetters.length)];
-    password += Passgen.digits[_random.nextInt(Passgen.digits.length)];
+    password += Passgen.words[Passgen._random.nextInt(Passgen.words.length)];
+    password += Passgen.upperLetters[Passgen._random.nextInt(Passgen.upperLetters.length)];
+    password += Passgen.digits[Passgen._random.nextInt(Passgen.digits.length)];
     return password;
   }
 }
